4d4e3d53b9364aa968e7afe7d9f769f4d8cbeed8
David (Pololu) <dev-david@pololu.com>
2017-02-23 10:41:37 -0800

    PowerDetect demo: Fixed a comment about the power switching circuit.

--- examples/PowerDetect/PowerDetect.ino
+++ examples/PowerDetect/PowerDetect.ino
@@ -6,11 +6,10 @@
 // The results are printed to the LCD and also to the serial
 // monitor.
 //
-// The battery voltage can only be read when the power switch is
-// in the "On" position.  If the power switch is off, the voltage
-// reading displayed by this demo will be low, but it might not
-// be zero because the Romi 32U4 has capacitors that take a while
-// to discharge.
+// The battery voltage can only be read when the power switching
+// circuit is on.  Otherwise, the voltage reading displayed by
+// this demo will be low, but it might not be zero because the
+// Romi 32U4 has capacitors that take a while to discharge.
 
 #include <Romi32U4.h>
 
